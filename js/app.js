6beb3675fee79a2bfeb631cf3b0d3b9b40ea902e
FraBona <francescobonafede08@gmail.com>
2023-10-24 17:07:32 +0200

    insert image from vue

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('ciao');
+const createApp = Vue.createApp
+
+createApp({
+  data(){
+    return{
+      images: [
+        "img/01.webp",
+        "img/02.webp",
+        "img/03.webp",
+        "img/04.webp",
+        "img/05.webp"
+      ],
+      title: [
+        "Spiderman",
+        "Ratchet & Clank:",
+        "Fortnite",
+        "Stray",
+        "Marvel's Avengers"
+      ],
+      currentIndex: 0,
+    }
+  },
+  methods: {
+    nextImage(){
+      this.currentIndex++;
+      console.log(this.currentIndex);
+    }
+  }
+}).mount('#app');
